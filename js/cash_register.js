060b40cd470e0dd2c3e9d773a27fabd16b45b73e
Sidney <user@SGD-MBP.local>
2016-06-09 22:37:25 -1000

    cash ragister

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,24 +1,34 @@
 var mainDiv = document.getElementById('maindiv');
 
+var daNumbas = [7,8,9,"%","CLR",4,5,6,"X","GtBl",1,2,3,"-","Dep$",0,"00",".","+","Wth$","="];
 function cash_register(p0){
 
     displayDiv = document.createElement('div');
     displayDiv.className = "display";
-    displayDiv.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit minima veniam sapiente, inventore itaque explicabo iste omnis autem et laboriosam, incidunt. Itaque perferendis voluptatum provident quidem accusamus, qui odio voluptatem!';
+    displayDiv.innerHTML = 'THE DISPLAY';
     mainDiv.appendChild(displayDiv);
 
-
-    var x = 9;
-    for (var i = 7; i<=x; i++){
-    calButtonDiv = document.createElement('block');
-    calButtonDiv.className = "calButtonDivs" + i;
-    calButtonDiv.innerHTML = i;
+    var x = 21;
+    for (var i = 1; i<=x; i++){
+    calButtonDiv = document.createElement('div');
+    calButtonDiv.className = "calButtonDivs";
+    calButtonDiv.id = "CBD"+i;
     mainDiv.appendChild(calButtonDiv);
+        if(i<22){
+            calButtonDiv.innerHTML = daNumbas[i-1];   
+        }
     }
+    
+    var clickable = document.getElementsByClassName('calButtonDivs');
+        for(var i = 0; i<clickable.length; i++){                       // Another happy 'for loop'.
+                clickable[i].addEventListener('click',theAdder[i]);   
+        }
 
+    var theAdder = [];
 
-
-
+    function clicktest(){
+        console.log("click!");       
+    }
 }
 
 cash_register();
